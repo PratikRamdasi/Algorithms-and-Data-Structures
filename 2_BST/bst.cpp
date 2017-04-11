a001996dd95c8bb578e839e2b9e39e6a64ad19f2
Pratik M Ramdasi <pratikramdasi1@gmail.com>
2017-04-10 20:32:09 -0600

    Added more operations to BST class

--- 2_BST/bst.cpp
+++ 2_BST/bst.cpp
@@ -1,4 +1,11 @@
-// Binary Search Tree in C++
+/*
+ * Author: Pratik Ramdasi
+ * 
+ * Date: 02/ 08/ 2017 
+ * 
+ * Title: Binary Search Tree Implementation in C++
+ * 
+ */
 
 #include <iostream>
 #include <cstddef>
@@ -30,35 +37,51 @@ class Operations {
 				return new Node(data);
 			}
 			else {
-				Node *cur;
-				if (data < cur->data) {
-					cur = addNode(root->left, data); 
-					root->left = cur;
+				if (data < root->data) {
+					root->left = addNode(root->left, data); 
 				}
 				else {
-					cur = addNode(root->right, data);
-					root->right = cur;
+					root->right = addNode(root->right, data);	
 				}
 				return root;
 			}
 		}
 		
-		// get height of the BST
+		// get height of the Tree - O(log n) when balanced , O(n) for unbalanced
 		int getHeight(Node *root)
 		{
 			int rightHeight = 0;
 			int leftHeight = 0;
-			
+			// if no tree
+			if (root == NULL) return 0;
+			// height of left subtree
 			if (root->left != NULL)  {
-				leftHeight = getHeight(root->left) + 1;
+				leftHeight = getHeight(root->left);
 			}
-			
+			// height of right subtree
 			if (root->right != NULL)  {
-				rightHeight = getHeight(root->right) + 1;
+				rightHeight = getHeight(root->right);
 			}
-			
-			return (leftHeight > rightHeight ? leftHeight : rightHeight);
+			// return max height
+			return (max(leftHeight, rightHeight) + 1);
 		
+		}
+		
+		/* pre-order traversal in a tree */
+		void preOrder(Node *root)
+		{
+			//print root element 
+			if (root != NULL) { 
+				cout << root->data << " ";
+			}
+			//print left subtree elements by recursion
+			if (root->left != NULL) {
+				preOrder(root->left);
+			}
+			//print right subtree elements by recursion
+			if (root->right != NULL) {
+				preOrder(root->right);
+			}
 		}
 		
 		// display BST
@@ -81,6 +104,41 @@ class Operations {
 			}
 			cout << endl;
 		}
+		
+		// Find largest element in BST - Rightmost element - O(h) time complexity
+		int findLargest(Node *root)
+		{
+			Node *largest = NULL;
+			while (root != NULL) 
+			{
+				if (!root->right) 
+				{
+					largest = root;
+				}
+				root = root->right;
+			}
+			return largest->data;
+		}
+		
+		// Find second largest element in the BST  -  O(h) time complexity, O(1) space complexity
+		int findSecondLargest(Node *root)
+		{
+			while(root) {
+				//case 1: righmost node has left subtree
+				if (root->left && !root->right) {
+					return findLargest(root->left);
+				}
+			
+				//case 2: current (root) is parent of rightmost child - current is second largest
+				if (root->right && !root->right->left && !root->right->right) {
+					break;
+				}
+				
+				// case 3: right subtree continues
+				root = root->right;
+			}
+			return root->data;
+		}
 };
 
 int main()
@@ -98,9 +156,18 @@ int main()
 	
 	// display tree by levels
 	myTree.levelOrder(root);
+	
 	// display tee height
 	int height = myTree.getHeight(root);
 	cout << "Height of BST is: " << height << endl;
+	
+	// Display largest element in the BST
+	int largest = myTree.findLargest(root);
+	cout << "Largest element in BST is: " << largest << endl;
+	
+	// Display second largest element in the BST
+	int slargest = myTree.findSecondLargest(root);
+	cout << "Second largest element in BST is: " << slargest << endl;	
 	
 	return 0;
 }
